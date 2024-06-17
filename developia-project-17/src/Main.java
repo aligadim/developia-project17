b6a234f04a7c28e7ed861af56588f1d5b63c2b38
aligadim <aliqedimov894@gmail.com>
2024-06-18 01:18:39 +0400

    ders3

---
+++ developia-project-17/src/Main.java
@@ -0,0 +1,55 @@
+@ -0,0 +1,54 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 5;
+        int j = 6;
+        int a = i+j;
+        System.out.println(a);
+
+        boolean student = true;
+        System.out.println(student);
+
+        char c = 'A';
+        System.out.println(c);
+
+        float f = 250000.80F;
+        System.out.println(f);
+
+
+        long l = 5000000L;
+        System.out.println(l);
+
+        short s = 100;
+        System.out.println(s);
+
+        double d = 30000.500;
+        System.out.println(d);
+
+        byte b1 = 120;
+        System.out.println(b1);
+
+        String a_s = "Samir Miskinov";
+        System.out.println(a_s);
+
+
+        String ad = "Hesen ";
+        String soyad = "Memmedov";
+        String br = ad+soyad;
+        System.out.println(br);
+
+        String ad1 = "Baxisov";
+        String soyad1 = "Memmed";
+        int ee = 15;
+        String il = "ilden cox";
+        String is = "Muellim isleyir";
+        System.out.println(ad1+" "+" "+soyad1+" "+" "+ee+" "+il+" "+is);
+
+
+
+
+
+    }
+}
