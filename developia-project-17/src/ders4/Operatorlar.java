f643c1236c874e0981284a0b8f0f45945b418165
aligadim <aliqedimov894@gmail.com>
2024-06-20 15:38:12 +0400

    ders 4

---
+++ developia-project-17/src/ders4/Operatorlar.java
@@ -0,0 +1,42 @@
+package ders4;
+
+public class Operatorlar {
+    public static void main(String[] args) {
+      byte myAge=98;
+      short myShort =myAge;
+
+      short a = -150;
+      byte b = (byte) a;
+        System.out.println(b);
+
+        String setir ="765846486";
+        int m = Integer.parseInt(setir);
+        System.out.println(m);
+
+        int k = 8956484;
+        String o = ""+k;
+        System.out.println(o);
+
+        a=6;
+        b=7;
+
+        int cem = a+b;
+        System.out.println(cem);
+        int ferq = a-b;
+        System.out.println(ferq);
+        int hasil= a*b;
+        System.out.println(hasil);
+        double q = 8;
+        double bol = q/b;
+        System.out.println(bol);
+        int i1 = 23;
+        int i2 = 11;
+        System.out.println(i1==i2);
+
+
+
+
+
+    }
+
+}
---
+++ developia-project-17/src/ders4/task.java
@@ -0,0 +1,40 @@
+package ders4;
+
+public class task {
+    public static void main(String[] args) {
+        int age = 400;
+        short myAge= (short) age;
+        System.out.println("1-ci "+myAge);
+
+        short sh = 69;
+        byte bt = (byte) sh;
+        System.out.println("2-ci "+bt);
+
+        short st = 589;
+        byte byt =(byte) st;
+        System.out.println("3-ci "+byt);
+
+        long lg = 458;
+        int i = (int) lg;
+        System.out.println("4-ci "+i);
+
+        long l= 52523635483L;
+        int in = (int) l;
+        System.out.println("5-ci "+in);
+
+        double d = 5632.6;
+        float f = (float) d;
+        System.out.println("6-ci "+f);
+
+        char c = 'D';
+        int ch =  c;
+        System.out.println("7-ci "+ch);
+
+        int dn = 453;
+        char cr = (char)dn;
+        System.out.println("8-ci "+cr);
+
+    }
+
+
+}
